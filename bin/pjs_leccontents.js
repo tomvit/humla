0f9eea567841b99dbd05ef232f2530a1a5bb267b
Tomas Vitvar <tomas@vitvar.com>
2017-03-14 18:45:35 +0100

    toc generator

---
+++ bin/pjs_leccontents.js
@@ -0,0 +1,50 @@
+// phantomjs script to retrieve sections data by using humla class model
+
+var system = require('system'),
+    page = require('webpage').create();
+
+if (system.args.length === 1) {
+	console.log("Usage: get-sections.js <lecture-file>");
+	phantom.exit(1);
+} else {
+	var lecturefile = system.args[1];
+
+	page.open(lecturefile, function(status) {
+		if (status === "success") {
+			var getSectionsData = function() {
+				var data = evaluate(page, function(lf) {
+    					if (humla && humla.root) {
+                                                humla.baseURI = lf;
+                                                return JSON.stringify(humla.root.contents);
+                                        } else
+                                                return null;
+  				}, lecturefile);
+
+				/*var data = page.evaluate(function() {
+					if (humla && humla.root) {
+						humla.baseURI = system.args[1];
+						return JSON.stringify(humla.root.contents);
+					} else
+						return null;
+				});*/
+				if (!data)
+					setTimeout(getSectionsData, 200);
+				else {
+					console.log(data);
+					phantom.exit();
+				}
+			};
+			setTimeout(getSectionsData, 200);
+		} else {
+			console.log(status);
+			phantom.exit(1);
+		}
+	} );
+}
+
+function evaluate(page, func) {
+    var args = [].slice.call(arguments, 2);
+    var fn = "function() { return (" + func.toString() + ").apply(this, " + JSON.stringify(args) + ");}";
+    return page.evaluate(fn);
+}
+
