3c1739942bb2d5803dbf234596fb94e074a45f62
tomvit <tomas@vitvar.com>
2021-09-25 22:47:23 +0200

    publish gh pages

---
+++ docker/image/scripts/github-webhook.js
@@ -0,0 +1,91 @@
+// github-webhook service 
+// @author: Tomas Vitvar, tomas@vitvar.com
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const child_process = require('child_process');
+
+const app = express()
+const port = 8080
+
+var jsonParser = bodyParser.json()
+
+function run_script_stream(req, res, stream_response) {
+    console.log("Starting process webhook /opt/humla/scripts/publish-gh-pages.sh");
+    var child = child_process.spawn("/opt/humla/scripts/publish-gh-pages.sh");
+
+    var scriptOutput = "";
+
+    if (stream_response) {
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.setHeader('Transfer-Encoding', 'chunked');
+    }
+
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function(data) {
+        process.stdout.write(data);
+	if (stream_response)
+	  res.write(data);    
+        data=data.toString();
+        scriptOutput+=data;
+    });
+
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function(data) {
+        process.stdout.write(data);
+	if (stream_response)
+	  res.write(data);
+        data=data.toString();
+        scriptOutput+=data;
+    });
+
+    child.on('close', function(code) {
+	console.log("The script finished, the exit code="+code)
+	if (stream_response)
+	  res.end()    
+	else 
+	  res.send(scriptOutput)
+    });
+}
+
+function run_script(req, res) {
+  console.log("Running webhook /opt/humla/scripts/publish-gh-pages.sh")	
+  exec('/opt/humla/scripts/publish-gh-pages.sh', (err, stdout, stderr) => {
+    if (err) {
+      console.log("ERROR: Cannot execute the command!");
+      console.log(`${stderr}`);
+      console.log(`${stdout}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    res.send("<pre>" + stdout + "</pre>")
+  });
+}
+
+let reqLogger = (req, res, next) => {
+  let dt = new Date();
+  let fdt = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();	 
+  console.log(`*** [${fdt}]: ${req.method} ${req.url}`);
+  next();
+};
+
+app.use(reqLogger);
+
+app.post('/humla/', jsonParser, (req, res) => {
+  console.log("Received WebHook request for " + req.body.ref)
+  branchName = req.body.ref.split('/').slice(-1)[0]	
+  if (branchName == "master") {
+    run_script(req, res);
+  } else {
+    console.log("No action for branch " + branchName + ".")
+  }
+})
+
+app.get('/humla/', (req, res) => {
+   run_script_stream(req, res, true);
+})
+
+app.listen(port, () => {
+  console.log(`GitHub WebHook service listening at http://localhost:${port}`)
+})
---
+++ docker/image/scripts/publish-gh-pages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# @author: Tomas Vitvar, tomas@vitvar.com
+# public humla course to gh-pages
+
+pdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+
+[ -z ${GITHUB_REPO} ] && echo "ERROR: The env variable \$GITHUB_REPO must be defined!" && exit 1
+[ -z ${GITHUB_TOKEN} ] && echo "ERROR: The env variable \$GITHUB_TOKEN must be defined!" && exit 1
+
+BUILD_DIR="$pdir/workspace/$(echo $GITHUB_REPO | awk -F/ '{print $NF}')"
+
+rm -fr ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+
+git clone https://${GITHUB_TOKEN}@${GITHUB_REPO} ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+git submodule update --init --recursive
+git config --global user.email "humla-build"
+git config --global user.name "humla-build"
+git checkout gh-pages
+git fetch
+git merge origin/master --no-commit || true
+
+export COURSE_HOME=$(pwd)
+humla/bin/generate-toc.sh $USECACHE
+humla/bin/generate-pdfs.sh $USECACHE
+
+git add -A .
+git commit -a -m "Humla build" || true
+git push --quiet origin gh-pages > /dev/null 2>&1
+
