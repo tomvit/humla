ad15ef62624d1711d38496f181ebab281dbe9731
tomvit <tomas@vitvar.com>
2021-09-26 21:49:31 +0200

    webhook using worker

--- docker/image/scripts/github-webhook.js
+++ docker/image/scripts/github-webhook.js
@@ -3,74 +3,62 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const child_process = require('child_process');
+const { Worker } = require('worker_threads')
 
 const app = express()
 const port = 8080
 
 var jsonParser = bodyParser.json()
 
-function run_script_stream(req, res, stream_response) {
-    console.log("Starting process webhook /opt/humla/scripts/publish-gh-pages.sh");
-    var child = child_process.spawn("/opt/humla/scripts/publish-gh-pages.sh");
+var num_running = 0
+var run_again = false
 
-    var scriptOutput = "";
-
-    if (stream_response) {
-      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-      res.setHeader('Transfer-Encoding', 'chunked');
-    }
-
-    child.stdout.setEncoding('utf8');
-    child.stdout.on('data', function(data) {
-        process.stdout.write(data);
-	if (stream_response)
-	  res.write(data);    
-        data=data.toString();
-        scriptOutput+=data;
-    });
-
-    child.stderr.setEncoding('utf8');
-    child.stderr.on('data', function(data) {
-        process.stdout.write(data);
-	if (stream_response)
-	  res.write(data);
-        data=data.toString();
-        scriptOutput+=data;
-    });
-
-    child.on('close', function(code) {
-	console.log("The script finished, the exit code="+code)
-	if (stream_response)
-	  res.end()    
-	else 
-	  res.send(scriptOutput)
-    });
-}
+if (!process.env.GITHUB_REPO)
+  throw "The env variable GITHUB_REPO is not defined!"
+  
+var repo_name = process.env.GITHUB_REPO.split("/").slice(-1)[0]  
 
 let reqLogger = (req, res, next) => {
   let dt = new Date();
-  let fdt = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();	 
-  console.log(`*** [${fdt}]: ${req.method} ${req.url}`);
-  next();
-};
+  let fdt = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + 
+    " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds() 
+  console.log(`*** [${fdt}]: ${req.method} ${req.url}`)
+  next()
+}
 
-app.use(reqLogger);
+app.use(reqLogger)
 
-app.post('/humla/', jsonParser, (req, res) => {
+function run_worker() {
+  var worker = new Worker('./worker.js')
+  num_running++
+  worker.on('exit', (code) => {
+    num_running--
+    if (run_again) {
+      run_again = false
+      run_worker()
+    }
+  })      
+}
+
+app.post('/humla/'+repo_name, jsonParser, (req, res) => {
   console.log("Received WebHook request for " + req.body.ref)
   branchName = req.body.ref.split('/').slice(-1)[0]	
-  if (branchName == "master") {
-    run_script_stream(req, res, false);
+  if (branchName == "master") {  
+    if (num_running == 0) {
+      run_again=false
+      run_worker()
+      res.send('The worker started.')
+    } else
+      if (!run_again) {
+        run_again=true
+        res.send("The worker is already running and it was shcedulled to run one more time.")
+      } else 
+        res.send("The worker is already running and is already scheduled to run again.") 
   } else {
     console.log("No action for branch " + branchName + ".")
   }
 })
 
-app.get('/humla/', (req, res) => {
-   run_script_stream(req, res, true);
-})
-
 app.listen(port, () => {
-  console.log(`GitHub WebHook service listening at http://localhost:${port}`)
+  console.log(`GitHub WebHook service serving ${process.env.GITHUB_REPO} is listening at http://localhost:${port}`)
 })
